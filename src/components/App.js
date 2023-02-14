58d186e21556ab3218847220d5c36da7f51613e9
NabiL Ashbat <nbl.dev21@gmail.com>
2023-02-14 20:32:32 +0300

    Discussion completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import SideMenu from "./SideMenu";
+import MainContent from "./MainContent";
+import TopMenu from "./TopMenu";
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+    return <div className="app-container">
+        {/* What components go here? */}
+        <SideMenu/>
+        <TopMenu/>
+        <MainContent/>
+    </div>;
 }
 
 export default App;
